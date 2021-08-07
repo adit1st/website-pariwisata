1a2ae67bb846ca173a2ae95d41fa3f24bf243bde
Adit1st <adityarizkypm@gmail.com>
2021-08-07 09:45:19 +0800

    Membuat form persyaratan

--- index.php
+++ index.php
@@ -95,7 +95,9 @@
         <div class="card-body">
         <h5 class="card-title">Danau Segara Anak</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-success">Lakukan Tour</a>
+        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#ModalPersyaratan">
+        Lakukan Tour
+        </button>
         </div>
         </div>
       </div>
@@ -105,7 +107,9 @@
         <div class="card-body">
         <h5 class="card-title">Gili Nanggu</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-success">Lakukan Tour</a>
+        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#ModalPersyaratan">
+        Lakukan Tour
+        </button>
         </div>
         </div>
       </div>
@@ -115,7 +119,9 @@
         <div class="card-body">
         <h5 class="card-title">Gunung Rinjani</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-success">Lakukan Tour</a>
+        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#ModalPersyaratan">
+        Lakukan Tour
+        </button>
         </div>
         </div>
       </div>
@@ -132,7 +138,9 @@
         <div class="card-body">
         <h5 class="card-title">Pantai Sekotong</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-success">Lakukan Tour</a>
+        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#ModalPersyaratan">
+        Lakukan Tour
+        </button>
         </div>
         </div>
       </div>
@@ -142,7 +150,9 @@
         <div class="card-body">
         <h5 class="card-title">Pantai Senggigi</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-success">Lakukan Tour</a>
+        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#ModalPersyaratan">
+        Lakukan Tour
+        </button>
         </div>
         </div>
       </div>
@@ -152,7 +162,9 @@
         <div class="card-body">
         <h5 class="card-title">Taman Narmada</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-success">Lakukan Tour</a>
+        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#ModalPersyaratan">
+        Lakukan Tour
+        </button>
         </div>
         </div>
       </div>
@@ -165,12 +177,43 @@
 <div class="mt-4">
 <div class="card text-center">
   <div class="card-body">
-    <h5 class="card-title">Contact Person <i class="bi bi-envelope"></i> : adityarizkypm@gmail.com</h5>
+    <h5 class="card-title">Email <i class="bi bi-envelope"></i> : adityarizkypm@gmail.com</h5>
     <p class="card-text">Copyright &#169; Adit1st 2021</p>
   </div>
 </div>
 </div>
 </Footer>
+
+<!-- Modal -->
+<div class="modal fade" id="ModalPersyaratan" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Form Persyaratan</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       <form>
+         <div class="row">
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Nama Depan" aria-label="First name">
+          </div>
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Nama Belakang" aria-label="Last name">
+          </div>
+         </div>
+         <div class="mt-3">
+          <input type="email" class="form-control" placeholder="Alamat Email" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+       </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
+        <button type="button" class="btn btn-primary">Simpan</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
